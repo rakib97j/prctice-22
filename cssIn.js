948a2ca6541eb9806308e174d53ccbf75c858bf0
rakib97j <rakibhassan23012007@gmail.com>
2026-02-15 22:58:12 +0600

    push

--- cssIn.js
+++ cssIn.js
@@ -5,4 +5,22 @@ for (let show of find) {
 
   show.style.color = "#ffffff96 ";
   show.style.backgroundColor = "#F54927";
+  show.style.padding ="20px"
+  show.style.borderRadius="20px"
+  show.style.width="500px"
+  show.style.fontSize="50px"
+  show.style.margin="50px 50px "
+  show.style.fontWidth="700"
+  show.style.border="50px solid green"
+  show.style.textAlign="right "
+  
+
+
 }
+
+// let cnt =document.querySelectorAll("#center");
+// for (let show2 of cnt){
+//     console.log(show2);
+
+//     show2.style.alignItem = "center";
+// }
