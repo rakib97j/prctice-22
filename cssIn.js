340810aee8cf7fa58e5f3a58edf9f6a661e87f3e
rakib97j <rakibhassan23012007@gmail.com>
2026-02-15 20:19:58 +0600

    push

---
+++ cssIn.js
@@ -0,0 +1,8 @@
+// console.log("rakib hassin")
+let find = document.querySelectorAll(".First-op");
+for (let show of find) {
+  console.log('show');
+
+  show.style.color = "#ffffff96 ";
+  show.style.backgroundColor = "#F54927";
+}
